e364af3c4f6f3227adceb16a351ae1bb9984f603
Your Name <NIGHCODE21>
2026-08-20 17:38:31 +0530

    Fix test

--- math_test.go
+++ math_test.go
@@ -2,7 +2,7 @@ package main
 import "testing" 
 func TestAdd(t *testing.T) { 
     result := Add(5, 3) 
-    expected := 10
+    expected := 8
     if result != expected { 
         t.Errorf("Expected %d but got %d", expected, result) 
     } 
